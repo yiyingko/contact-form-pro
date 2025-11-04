edcfa68590d47cd756cf365b4319df1f63daa9d1
YiYing Ko <ykocuriosity@gmail.com>
2025-11-04 15:15:01 +0000

    chore: add pre-commit hook script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+set -e
+
 echo "🧹 Running ESLint and Prettier before commit..."
+npm run lint:fix
+npm run format
 
-npm run lint:fix && npm run format
-
-if [ $? -ne 0 ]; then
-  echo "❌ Commit aborted: Lint or format failed."
-  exit 1
-fi
+echo "✅ Pre-commit checks passed."
